fd58686af1211ce0d2088f2d635ab04854628cf9
Blörg <devel@bloerg.de>
2015-12-08 19:24:43 +0100

    rm all beams before compiling again

--- run_all_tests.bash
+++ run_all_tests.bash
@@ -5,6 +5,7 @@ if [ ! -d "$sine_spectra_test_directory" ] ; then
   echo "Error: cannot run tests."
   echo "Please mkdir /var/tmp/sine_spectra_test_directory for testing purposes!"
 else
+    rm ebin/*.beam
     erlc -o ebin/ src/*.erl test/*.erl
     erl -pa ebin/ -noshell -s run_all_tests start -s init stop
 fi
