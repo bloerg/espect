579f6777b8899e24bb630ccf4a574e1bf8d1a632
Blörg <devel@bloerg.de>
2015-12-09 12:36:22 +0100

    benchmark for neuron update function

--- src/benchmark.erl
+++ src/benchmark.erl
@@ -1,5 +1,7 @@
 -module(benchmark).
 -export([speed_test_get_neuron_spectrum_distance/2]).
+-export([speed_test_update_neuron/2]).
+
 
 
 speed_test_get_neuron_spectrum_distance(Spectrum_width, Repetitions) ->
@@ -9,7 +11,7 @@ speed_test_get_neuron_spectrum_distance(Spectrum_width, Repetitions) ->
     neuron:start({local, benchmark_test_neuron}, [1,2], Neuron, [], 0),
     lists:foreach(
         fun(_Iteration) ->
-            neuron:get_neuron_spectrum_distance(test, Spectrum, [1,2])
+            neuron:get_neuron_spectrum_distance(benchmark_test_neuron, Spectrum, [1,2])
         end,
         [X || X <- lists:seq(1,Repetitions)]
     ),
@@ -19,3 +21,19 @@ speed_test_get_neuron_spectrum_distance(Spectrum_width, Repetitions) ->
 
 .
  
+speed_test_update_neuron(Spectrum_width, Repetitions) ->
+    T1 = os:timestamp(),
+    Neuron = sample_spectra:make_sine_spectrum(Spectrum_width),
+    Spectrum = sample_spectra:make_sine_spectrum(Spectrum_width),
+    neuron:start({local, benchmark_test_neuron}, [1,2], Neuron, [], 0),
+    lists:foreach(
+        fun(_Iteration) ->
+            neuron:update_neuron(benchmark_test_neuron, Spectrum)
+        end,
+        [X || X <- lists:seq(1,Repetitions)]
+    ),
+    T2 = os:timestamp(),
+    erlang:display({debug, "time in microseconds: ", timer:now_diff(T2, T1), ", updates per second: ", Repetitions/timer:now_diff(T2,T1)*1000000}),
+    neuron:stop(benchmark_test_neuron)
+
+.
