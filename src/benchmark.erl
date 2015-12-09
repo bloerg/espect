286e5bed54ed98715a4c00a2dbee1b264be69ad0
Blörg <devel@bloerg.de>
2015-12-09 10:59:07 +0100

    updated function name and stop process after use

--- src/benchmark.erl
+++ src/benchmark.erl
@@ -1,19 +1,21 @@
 -module(benchmark).
--export([speed_test_compare_neuron_with_spectrum/2]).
+-export([speed_test_get_neuron_spectrum_distance/2]).
 
 
-speed_test_compare_neuron_with_spectrum(Spectrum_width, Repetitions) ->
+speed_test_get_neuron_spectrum_distance(Spectrum_width, Repetitions) ->
     T1 = os:timestamp(),
     Neuron = sample_spectra:make_sine_spectrum(Spectrum_width),
     Spectrum = sample_spectra:make_sine_spectrum(Spectrum_width),
     neuron:start({local, benchmark_test_neuron}, [1,2], Neuron, [], 0),
     lists:foreach(
         fun(_Iteration) ->
-            neuron:compare_neuron_with_spectrum(test, Spectrum, [1,2])
+            neuron:get_neuron_spectrum_distance(test, Spectrum, [1,2])
         end,
         [X || X <- lists:seq(1,Repetitions)]
     ),
     T2 = os:timestamp(),
-    erlang:display({debug, "time in microseconds: ", timer:now_diff(T2, T1), ", compares per second: ", Repetitions/timer:now_diff(T2,T1)*1000000})
+    erlang:display({debug, "time in microseconds: ", timer:now_diff(T2, T1), ", compares per second: ", Repetitions/timer:now_diff(T2,T1)*1000000}),
+    neuron:stop(benchmark_test_neuron)
+
 .
  
