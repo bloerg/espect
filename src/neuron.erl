08d51155eac4f5ed3b92422e2b9cc720e989dabc
Blörg <devel@bloerg.de>
2015-12-10 10:29:22 +0100

    consider max_iteration and BMU_coordinates in function calls

--- src/neuron.erl
+++ src/neuron.erl
@@ -1,31 +1,31 @@
 -module(neuron).
 -beaviour(gen_server).
 
--export([start/4, start_link/4, start/5, start_link/5]).
+-export([start/5, start_link/5, start/6, start_link/6]).
 -export([stop/0, stop/1, terminate/2]).
 -export([handle_cast/2]).
 -export([handle_call/3]).
 -export([init/1]).
 -export([get_neuron_spectrum_distance/3]).
 -export([set_bmu/2]).
--export([update_neuron/2]).
+-export([update_neuron/3]).
 
 %for testing, remove later
 -export([alpha/4, sigma/4, neighbourhood_function1/4]).
 
 
 %%Start a neuron server
-start(Server_name, Neuron_coordinates, Neuron_vector, BMU, Iteration) ->
-    gen_server:start(Server_name, ?MODULE, [Neuron_coordinates, Neuron_vector, BMU, Iteration], []).
+start(Server_name, Neuron_coordinates, Neuron_vector, BMU, Iteration, Max_iteration) ->
+    gen_server:start(Server_name, ?MODULE, [Neuron_coordinates, Neuron_vector, BMU, Iteration, Max_iteration], []).
 
-start_link(Server_name, Neuron_coordinates, Neuron_vector, BMU, Iteration) ->
-    gen_server:start(Server_name, ?MODULE, [Neuron_coordinates, Neuron_vector, BMU, Iteration], []).
+start_link(Server_name, Neuron_coordinates, Neuron_vector, BMU, Iteration, Max_iteration) ->
+    gen_server:start(Server_name, ?MODULE, [Neuron_coordinates, Neuron_vector, BMU, Iteration, Max_iteration], []).
 
-start(Neuron_coordinates, Neuron_vector, BMU, Iteration) ->
-    gen_server:start(?MODULE, [Neuron_coordinates, Neuron_vector, BMU, Iteration], []).
+start(Neuron_coordinates, Neuron_vector, BMU, Iteration, Max_iteration) ->
+    gen_server:start(?MODULE, [Neuron_coordinates, Neuron_vector, BMU, Iteration, Max_iteration], []).
 
-start_link(Neuron_coordinates, Neuron_vector, BMU, Iteration) ->
-    gen_server:start(?MODULE, [Neuron_coordinates, Neuron_vector, BMU, Iteration], []).
+start_link(Neuron_coordinates, Neuron_vector, BMU, Iteration, Max_iteration) ->
+    gen_server:start(?MODULE, [Neuron_coordinates, Neuron_vector, BMU, Iteration, Max_iteration], []).
 
 %%Stop a neuron server
 stop() ->
@@ -50,41 +50,45 @@ get_neuron_spectrum_distance(Server_name, Spectrum, Spectrum_metadata) ->
 set_bmu(Server_name, New_BMU) ->
     gen_server:call(Server_name, {set_bmu, New_BMU}).
 
-update_neuron(Server_name, BMU_spectrum) ->
-    gen_server:call(Server_name, {update_neuron, BMU_spectrum}).
+%% @doc calls the function to compute the new neuron vector
+update_neuron(Server_name, BMU_spectrum, BMU_coordinates) ->
+    gen_server:call(Server_name, {update_neuron, BMU_spectrum, BMU_coordinates}).
     
     
 
 handle_call(
-    {compare, Spectrum, Spectrum_metadata}, _From, [Neuron_coordinates, Neuron_vector, BMU, Iteration]) ->
+    {compare, Spectrum, Spectrum_metadata}, _From, [Neuron_coordinates, Neuron_vector, BMU, Iteration, Max_iteration]) ->
         Reply = [Neuron_coordinates, 
                  Spectrum_metadata, 
                  vector_operations:vector_distance(Neuron_vector, Spectrum)
                 ],
-        {reply, Reply, [Neuron_coordinates, Neuron_vector, BMU, Iteration]}
+        {reply, Reply, [Neuron_coordinates, Neuron_vector, BMU, Iteration, Max_iteration]}
     ;
 handle_call(
-    {set_bmu, New_BMU}, _From, [Neuron_coordinates, Neuron_vector, _Old_BMU, Iteration]) ->
-        {reply, ok, [Neuron_coordinates, Neuron_vector, New_BMU, Iteration]}
+    {set_bmu, New_BMU}, _From, [Neuron_coordinates, Neuron_vector, _Old_BMU, Iteration, Max_iteration]) ->
+        {reply, ok, [Neuron_coordinates, Neuron_vector, New_BMU, Iteration, Max_iteration]}
     ;
 
 handle_call(
-    {update_neuron, BMU_spectrum}, _From, [Neuron_coordinates, Neuron_vector, BMU, Iteration]) ->
-        T_max = 200, %%Fixme: function to get T_max (should be in neuron state?)
+    {update_neuron, BMU_spectrum, BMU_coordinates}, _From, [Neuron_coordinates, Neuron_vector, BMU, Iteration, Max_iteration]) ->
+        %% The if is for benchmark. Iteration should not go back to 0, actually
+        if
+            Iteration < Max_iteration -> Iteration2 = Iteration + 1;
+            Iteration == Max_iteration -> Iteration2 = 0
+        end,
         {reply, ok, [Neuron_coordinates, 
                  vector_operations:vector_sum(Neuron_vector,
                     vector_operations:scalar_multiplication(
-                        neighbourhood_function1(Iteration, T_max, Neuron_vector, BMU_spectrum),
+                        neighbourhood_function1(Iteration, Max_iteration, Neuron_coordinates, BMU_coordinates),
                         vector_operations:vector_difference(BMU_spectrum, Neuron_vector)
                     )
                  ),
                  BMU, 
-                 Iteration + 1
+                 Iteration2,                
+                 Max_iteration
                 ]
         }
-
 .
-
 
 
 
@@ -95,14 +99,14 @@ alpha(T, T_max, Alpha_begin, Alpha_end) ->
 sigma(T, T_max, Sigma_begin, Sigma_end) ->
     Sigma_begin * math:pow(Sigma_end / Sigma_begin, T/T_max).
 
-neighbourhood_function1(T, T_max, Neuron, BMU_spectrum) ->
+neighbourhood_function1(T, T_max, Neuron_coordinates, BMU_coordinates) ->
     Sigma_begin = 1.0,
     Sigma_end = 0.0625,
     Alpha_begin = 0.25,
     Alpha_end = 0.01,
     alpha(T, T_max, Alpha_begin, Alpha_end) * 
         math:exp(
-            -vector_operations:vector_distance(Neuron, BMU_spectrum) 
+            -vector_operations:vector_distance(BMU_coordinates, Neuron_coordinates) 
             / (2 * math:pow(
                             sigma(T, T_max, Sigma_begin, Sigma_end),
                             2
