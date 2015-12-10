e61d5a0f899f742ebf70bbdf462ccaa38879946c
Blörg <devel@bloerg.de>
2015-12-10 11:28:53 +0100

    provide a number of random sine spectra

--- src/spectrum_handling.erl
+++ src/spectrum_handling.erl
@@ -1,7 +1,7 @@
 -module(spectrum_handling).
 
 -export([read_spectrum_from/2]).
--export([provide_spectra/2]).
+-export([provide_spectra/2, provide_spectra/3]).
 
 % @doc Read and return a spectrum from a given source
 read_spectrum_from(filesystem, Path) ->
@@ -15,6 +15,7 @@ read_spectrum_from(random_sine, Spectrum_length) ->
     sample_spectra:make_sine_spectrum(Spectrum_length)
 .
 
+
 % @get a number of spectra from a given source
 provide_spectra(filesystem, Directory) ->
     case file:list_dir(Directory) of
@@ -23,3 +24,5 @@ provide_spectra(filesystem, Directory) ->
         {error, Reason} ->
             {error, Reason}
     end.
+provide_spectra(random_sine, Spectrum_length, Number_of_spectra) ->
+    [sample_spectra:make_sine_spectrum(Spectrum_length) || _X <- lists:seq(1, Number_of_spectra)].
