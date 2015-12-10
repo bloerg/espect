fb05e5e3589e69e223303942fe27e83c3de4a8bc
Blörg <devel@bloerg.de>
2015-12-10 14:04:53 +0100

    return file list for directory

--- src/spectrum_handling.erl
+++ src/spectrum_handling.erl
@@ -2,6 +2,7 @@
 
 -export([read_spectrum_from/2]).
 -export([provide_spectra/2, provide_spectra/3]).
+-export([provide_spectra_list/2]).
 
 % @doc Read and return a spectrum from a given source
 read_spectrum_from(filesystem, Path) ->
@@ -16,6 +17,7 @@ read_spectrum_from(random_sine, Spectrum_length) ->
 .
 
 
+
 % @get a number of spectra from a given source
 provide_spectra(filesystem, Directory) ->
     case file:list_dir(Directory) of
@@ -24,5 +26,17 @@ provide_spectra(filesystem, Directory) ->
         {error, Reason} ->
             {error, Reason}
     end.
+
 provide_spectra(random_sine, Spectrum_length, Number_of_spectra) ->
     [sample_spectra:make_sine_spectrum(Spectrum_length) || _X <- lists:seq(1, Number_of_spectra)].
+
+
+
+% @get a list of files in a directory
+provide_spectra_list(filesystem, Directory) ->
+    case file:list_dir(Directory) of
+        {ok, Files} ->
+            Files;
+        {error, Reason} ->
+            {error, Reason}
+    end.
