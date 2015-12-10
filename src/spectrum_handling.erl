112f333e61b1acd432648d317c2d937923b63cd3
Blörg <devel@bloerg.de>
2015-12-10 10:54:26 +0100

    read several spectra from directory

--- src/spectrum_handling.erl
+++ src/spectrum_handling.erl
@@ -1,6 +1,7 @@
 -module(spectrum_handling).
 
 -export([read_spectrum_from/2]).
+-export([provide_spectra/3]).
 
 % @doc Read and return a spectrum from a given source
 read_spectrum_from(filesystem, Path) ->
@@ -10,3 +11,11 @@ read_spectrum_from(random_sine, Spectrum_length) ->
     sample_spectra:make_sine_spectrum(Spectrum_length)
 .
 
+% @get a number of spectra from a given source
+provide_spectra(filesystem, Directory, Number_of_spectra) ->
+    case file:list_dir(Directory) of
+        {ok, Files} ->
+            [read_spectrum_from(filesystem, string:concat(Directory, File)) || File <- Files];
+        {error, Reason} ->
+            {error, Reason}
+    end.
