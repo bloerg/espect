ba0ac0bb706b8999b8ce6644db333810a4043a63
Blörg <devel@bloerg.de>
2015-12-10 11:04:03 +0100

    error handling for fileload and load all spectra in directory

--- src/spectrum_handling.erl
+++ src/spectrum_handling.erl
@@ -1,18 +1,22 @@
 -module(spectrum_handling).
 
 -export([read_spectrum_from/2]).
--export([provide_spectra/3]).
+-export([provide_spectra/2]).
 
 % @doc Read and return a spectrum from a given source
 read_spectrum_from(filesystem, Path) ->
-    {ok, Spectrum} = file:consult(Path),
-    hd(Spectrum);
+    case file:consult(Path) of
+        {ok, Spectrum} ->
+            hd(Spectrum);
+        {error, Reason} ->
+            {error, Reason}
+    end;
 read_spectrum_from(random_sine, Spectrum_length) ->
     sample_spectra:make_sine_spectrum(Spectrum_length)
 .
 
 % @get a number of spectra from a given source
-provide_spectra(filesystem, Directory, Number_of_spectra) ->
+provide_spectra(filesystem, Directory) ->
     case file:list_dir(Directory) of
         {ok, Files} ->
             [read_spectrum_from(filesystem, string:concat(Directory, File)) || File <- Files];
