ca52839baccc3c490a11e9df0ac5f00644769f50
Blörg <devel@bloerg.de>
2015-12-13 15:24:54 +0100

    keep the spectrum of the BMU neuron in state variable

--- src/bmu_manager.erl
+++ src/bmu_manager.erl
@@ -12,16 +12,16 @@
 %%Start a neuron server
 %returns [Iteration, Max_iteration, Shortest_distance, BMU_coordinates]
 start(Server_name, Iteration, Max_iteration) ->
-    gen_server:start(Server_name, ?MODULE, [Iteration, Max_iteration, 1000000, {}], []). 
+    gen_server:start(Server_name, ?MODULE, [Iteration, Max_iteration, 1000000, [], []], []). 
 
 start_link(Server_name, Iteration, Max_iteration) ->
-    gen_server:start_link(Server_name, ?MODULE, [Iteration, Max_iteration, 1000000, {}], []).
+    gen_server:start_link(Server_name, ?MODULE, [Iteration, Max_iteration, 1000000, [], []], []).
 
 start(Iteration, Max_iteration) ->
-    gen_server:start(?MODULE, [Iteration, Max_iteration, 1000000, {}], []).
+    gen_server:start(?MODULE, [Iteration, Max_iteration, 1000000, [], []], []).
 
 start_link(Iteration, Max_iteration) ->
-    gen_server:start(?MODULE, [Iteration, Max_iteration, 1000000 , {}], []).
+    gen_server:start(?MODULE, [Iteration, Max_iteration, 1000000 , [], []], []).
 
 %%Stop a neuron server
 stop() ->
@@ -41,7 +41,8 @@ neuron_spectrum_distance(Result_receiver_name, Data
     %~ % i. e.
     %~ [Neuron_coordinates, 
          %~ Spectrum_metadata, 
-         %~ Spectrum_neuron_distance
+         %~ Spectrum_neuron_distance,
+         %~ Neuron_vector
     %~ ]
     ) ->
         gen_server:cast(Result_receiver_name, Data).
@@ -53,17 +54,17 @@ get_bmu(Result_receiver_name) ->
 get_state(Result_receiver_name) ->
     gen_server:call(Result_receiver_name, get_state).
 
-handle_cast([Neuron_coordinates, _Spectrum_metadata, Spectrum_neuron_distance], [Iteration, Max_iteration, Shortest_distance, BMU_coordinates]) ->
+handle_cast([Neuron_coordinates, _Spectrum_metadata, Spectrum_neuron_distance, Neuron_vector], [Iteration, Max_iteration, Shortest_distance, BMU_coordinates, BMU_neuron_vector]) ->
     case Spectrum_neuron_distance < Shortest_distance of
-        true -> {noreply, [Iteration, Max_iteration, Spectrum_neuron_distance, Neuron_coordinates]};
-        false -> {noreply, [Iteration, Max_iteration, Shortest_distance, BMU_coordinates]}
+        true -> {noreply, [Iteration, Max_iteration, Spectrum_neuron_distance, Neuron_coordinates, Neuron_vector]};
+        false -> {noreply, [Iteration, Max_iteration, Shortest_distance, BMU_coordinates, BMU_neuron_vector]}
     end;
     
 handle_cast(stop, Neuron_state) ->
     {stop, normal, Neuron_state}.
     
 handle_call(get_bmu, _From, State) ->
-    [_Iteration, _Max_iteration, _Shortest_distance, BMU_coordinates] = State,
+    [_Iteration, _Max_iteration, _Shortest_distance, BMU_coordinates, _BMU_neuron_vector] = State,
     {reply, BMU_coordinates, State};
 handle_call(get_state, _From, State) ->
     {reply, State, State}.
