df0600d1782c4ee9df87ac1ff66ca134fc465a2c
Blörg <devel@bloerg.de>
2015-12-22 00:24:41 +0100

    deliver spectra only once in a iteration step, if all spectra delivered, return []

--- src/spectrum_dispatcher.erl
+++ src/spectrum_dispatcher.erl
@@ -6,7 +6,7 @@
 -export([handle_cast/2]).
 -export([handle_call/3]).
 -export([init/1]).
--export([get_spectrum/1, get_spectrum_with_id/1, set_iteration/2]).
+-export([get_spectrum/1, get_spectrum_with_id/1, set_iteration/2, get_minimum_som_dimensions/1, get_number_of_spectra/1]).
 %-export([update_iteration/1)].
 
 
@@ -53,7 +53,13 @@ init([{filesystem, Directory, File_format, Start_index}, Iteration, Max_iteratio
     }
 .
 
-
+get_minimum_som_dimensions_helper(Number_of_spectra) ->
+    get_minimum_som_dimensions_helper(1, Number_of_spectra).
+get_minimum_som_dimensions_helper(Edge_length, Number_of_spectra) ->
+    case Edge_length * Edge_length > Number_of_spectra of
+        true -> [Edge_length, Edge_length];
+        false -> get_minimum_som_dimensions_helper(Edge_length + 1, Number_of_spectra)
+    end.
 
 get_spectrum(Server_name) ->
     gen_server:call(Server_name, get_spectrum).
@@ -61,12 +67,15 @@ get_spectrum(Server_name) ->
 get_spectrum_with_id(Server_name) ->
     gen_server:call(Server_name, get_spectrum_with_id).
     
-set_iteration(Server_name, New_iteration) ->
-    gen_server:cast(Server_name, {set_iteration, New_iteration}).
-    
-handle_cast({set_iteration, New_iteration}, [Spectra_source, _Old_iteration, Max_iteration]) ->
-    {noreply, [Spectra_source, New_iteration, Max_iteration]};
+get_minimum_som_dimensions(Server_name) ->
+    gen_server:call(Server_name, get_minimum_som_dimensions).
 
+get_number_of_spectra(Server_name) ->
+    gen_server:call(Server_name, get_number_of_spectra).
+
+set_iteration(Server_name, New_iteration) ->
+    gen_server:call(Server_name, {set_iteration, New_iteration}).
+    
 handle_cast(stop, Spectrum_dispatcher_state) ->
     {stop, normal, Spectrum_dispatcher_state}.
 
@@ -93,21 +102,40 @@ handle_call(
                         }, Iteration, Max_iteration
                         ]
         };
+        
+% returns a spectrum from the filesystem as long as spectra are left
+% returns [] if all spectra where delivered in the current Iterations step
 handle_call(
     get_spectrum, _From, [{filesystem, Directory, File_format, Spectra_file_list, Spec_list_index}, Iteration, Max_iteration]) ->
-        [_Spectrum_id, Spectrum] = spectrum_handling:read_spectrum_from(
-            filesystem, 
-            string:concat(Directory, lists:nth(Spec_list_index, Spectra_file_list)), 
-            File_format
-        ),
-        {reply, Spectrum, [{filesystem, Directory, File_format, Spectra_file_list, 
-                            case Spec_list_index == length(Spectra_file_list) of
-                                true -> _Next_iteration = iteration_state_server:next_iteration(iteration_state_server),
-                                        1;
-                                false -> Spec_list_index +1 
-                            end
-                        }, Iteration, Max_iteration
-                        ]
-        }.
+        case Spec_list_index > length(Spectra_file_list) of 
+            false ->
+                [_Spectrum_id, Spectrum] = spectrum_handling:read_spectrum_from(
+                    filesystem, 
+                    string:concat(Directory, lists:nth(Spec_list_index, Spectra_file_list)), 
+                    File_format
+                );
+            true ->
+                Spectrum = []
+        end,
+        {reply, Spectrum, 
+            [{filesystem, Directory, File_format, Spectra_file_list, Spec_list_index +1}, 
+              Iteration, Max_iteration
+            ]
+        };
+
+handle_call(
+    get_minimum_som_dimensions, _From, [{filesystem, Directory, File_format, Spectra_file_list, Spec_list_index}, Iteration, Max_iteration]) ->
+        {reply, 
+         get_minimum_som_dimensions_helper(length(Spectra_file_list)), 
+         [{filesystem, Directory, File_format, Spectra_file_list, Spec_list_index}, Iteration, Max_iteration]
+        };
+handle_call(
+    get_number_of_spectra, _From, [{filesystem, Directory, File_format, Spectra_file_list, Spec_list_index}, Iteration, Max_iteration]) ->
+        {reply,
+         length(Spectra_file_list), 
+         [{filesystem, Directory, File_format, Spectra_file_list, Spec_list_index}, Iteration, Max_iteration]
+        };
+handle_call({set_iteration, New_iteration}, _From, [{filesystem, Directory, File_format, Spectra_file_list, _Spec_list_index}, _Iteration, Max_iteration]) ->
+    {reply, ok, [{filesystem, Directory, File_format, Spectra_file_list, 1}, New_iteration, Max_iteration]}.
 
 
