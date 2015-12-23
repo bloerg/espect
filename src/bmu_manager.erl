8f7c019e2f1470657b1897868e038cab8b4b543e
Blörg <devel@bloerg.de>
2015-12-23 19:00:22 +0100

    handle pid list with recursive function instead of lists:... functions

--- src/bmu_manager.erl
+++ src/bmu_manager.erl
@@ -5,9 +5,11 @@
 -export([stop/0, stop/1, terminate/2]).
 -export([handle_cast/2]).
 -export([handle_call/3]).
+-export([handle_info/2]).
 -export([init/1]).
 -export([neuron_spectrum_distance/2, get_bmu/1, get_state/1, set_iteration/2]).
 -export([set_neurons_worker_list/2, next_learning_step/0]).
+-export([remove_pid_from_list/2]).
 
 -record(bmu_manager_state, {
     bmu_coordinates = [], % The som_x, som_y coordinates of the best matching unit
@@ -90,6 +92,21 @@ filter_neurons_worker_list(Neurons_worker_list, Filtered_list) ->
     end.
 
 
+remove_pid_from_list([], _Pid) ->
+    [];
+remove_pid_from_list(Pid_list, Pid) ->
+    remove_pid_from_list(Pid_list, [], Pid).
+    
+remove_pid_from_list([], Filtered_pid_list, _Pid) ->
+    Filtered_pid_list;
+remove_pid_from_list(Pid_list, Filtered_pid_list, Pid) ->
+    [Head|Tail] = Pid_list,
+    case Head of
+        Pid -> remove_pid_from_list(Tail, Filtered_pid_list, Pid);
+        _Other -> remove_pid_from_list(Tail, [Head|Filtered_pid_list], Pid)
+    end.
+
+
 neuron_spectrum_distance(Result_receiver_name, Data
     %~ % i. e.
     %~ [Neuron_coordinates, 
@@ -114,22 +131,17 @@ set_neurons_worker_list(Server_name, Neurons_worker_list) ->
     gen_server:call(Server_name, {set_neurons_worker_list, Neurons_worker_list}).
 
 next_learning_step() ->
-    gen_server:cast(?MODULE, next_learning_step).
+    gen_server:call(?MODULE, next_learning_step).
 
-handle_cast({set_iteration, New_iteration}, BMU_manager_state) ->
-    {noreply, BMU_manager_state#bmu_manager_state{iteration = New_iteration} };
-handle_cast(next_learning_step, BMU_manager_state) ->
-    {noreply, 
+    
+handle_call(next_learning_step, _From, BMU_manager_state) ->
+    {reply, ok, 
         BMU_manager_state#bmu_manager_state{
             bmu_coordinates = [],
             bmu_spectrum_metadata = [],
             shortest_distance = 576460752303423487
         }
     };
-    
-handle_cast(stop, Neuron_state) ->
-    {stop, normal, Neuron_state}.
-    
 handle_call({set_neurons_worker_list, Neurons_worker_list}, _From, BMU_manager_state) ->
     {reply, ok, BMU_manager_state#bmu_manager_state{neurons_worker_list = filter_neurons_worker_list(Neurons_worker_list)}};
 handle_call(get_bmu, _From, BMU_manager_state) ->
@@ -140,13 +152,10 @@ handle_call(get_state, _From, BMU_manager_state) ->
 handle_call({intermediate, [Neuron_coordinates, Spectrum_id, Spectrum_neuron_distance]}, From,
     BMU_manager_state) ->
         {From_pid, _From_tag} = From,
-        % remove the pid of the sending neurons worker from the list of
-        % registered workers. This is to keep track of the responses of the neuron workers
-        Neurons_worker_list_new = lists:takewhile(
-            fun(Pid) -> Pid =/= From_pid end, 
-            BMU_manager_state#bmu_manager_state.neurons_worker_list
+        Neurons_worker_list_new = remove_pid_from_list(
+            BMU_manager_state#bmu_manager_state.neurons_worker_list,
+            From_pid
         ),
-        
         case Spectrum_neuron_distance < BMU_manager_state#bmu_manager_state.shortest_distance of
             true -> 
                 New_bmu_manager_state = 
@@ -166,17 +175,23 @@ handle_call({intermediate, [Neuron_coordinates, Spectrum_id, Spectrum_neuron_dis
         
         case length(Neurons_worker_list_new) of
             0 ->
-                ok = neurons:set_bmu(
+                ok = learning_step_manager:compare_complete(
                     From_pid, 
-                    New_bmu_manager_state#bmu_manager_state.bmu_coordinates, 
+                    New_bmu_manager_state#bmu_manager_state.bmu_coordinates,
                     New_bmu_manager_state#bmu_manager_state.bmu_spectrum_metadata
-                ),
-                ok = learning_step_manager:compare_complete();
-            Other_number -> 
-                erlang:display(Other_number),
+                );
+            _Other_number -> 
                 ok
         end,
         
         {reply, ok, New_bmu_manager_state}.
+    
+handle_cast({set_iteration, New_iteration}, BMU_manager_state) ->
+    {noreply, BMU_manager_state#bmu_manager_state{iteration = New_iteration} };
 
+handle_cast(stop, Neuron_state) ->
+    {stop, normal, Neuron_state}.
+
+handle_info(Message, State) ->
+    io:format("Unexpected message: ~w~n",[{Message, State}]).
 
